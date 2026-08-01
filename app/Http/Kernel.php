f7ce47a8b702515b879849a897fb98f00e65ddb2
Vun Thy <thyvonny@gmail.com>
2026-08-01 22:52:17 +0700

    middleware: fill SERVER_NAME/SCRIPT_NAME/etc missing from Octane's $_SERVER so vendor code (theme colors, paystack, mpdf) keeps working

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \App\Http\Middleware\EnsureServerGlobals::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
